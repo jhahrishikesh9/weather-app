05a99718daf17a4657fbcdf48cf519587b1160fc
jhahrishikesh9 <68316230+jhahrishikesh9@users.noreply.github.com>
2022-02-10 11:16:47 +0530

    Add files via upload

---
+++ Rest_project/script.js
@@ -0,0 +1,39 @@
+let weather = {
+    "apikey" : "edd2f22bfb3bcb18595a8ae5207d7d87",
+    fetchWeather: function(city){
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=metric&appid="+this.apikey)
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data){
+        const {name} = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon,description,temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in "+name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon +".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText =temp+"°C";
+        document.querySelector(".humidity").innerText ="Humidity is "+humidity+"%";
+        document.querySelector(".speed").innerText ="Speed is "+speed + " kmph";
+        document.querySelector(".weather").classList.remove("loading");
+        
+    },
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+
+
+};
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+
+});
+document.querySelector(".search-bar").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("kolkata");
